6f65131059d339fcefda2aeea79eef31c4e9a8f5
Mateusz Łątka <mateusz.latka9@gmail.com>
2021-04-16 11:42:39 +0200

    update

--- src/main/java/com/example/quizproject/configuration/QuizConfig.java
+++ src/main/java/com/example/quizproject/configuration/QuizConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -17,80 +18,23 @@ public class QuizConfig {
     CommandLineRunner commandLineRunner(QuizQuestionRepository quizQuestionRepository) {
         return args -> {
 
-            QuizQuestion quizQuestion1 = new QuizQuestion(
-                    "Ile jest 2+2?",
-                    3,
-                    List.of(
-                            "16",
-                            "1",
-                            "0",
-                            "4"
-                    ),
-                    "Matematyka");
+            List<QuizQuestion> list = new ArrayList<>();
 
-            QuizQuestion quizQuestion2 = new QuizQuestion(
-                    "Jaki jest wzór na pole kwadratu",
-                    0,
-                    List.of(
-                            "a^2",
-                            "a^3",
-                            "a+b",
-                            "(a+b)^2"
-                    ),
-                    "Matematyka");
+            list.add(new QuizQuestion(
+                    "Ile jest 2+2?", 3, List.of("16", "1", "0", "4"), "matematyka"));
 
-            QuizQuestion quizQuestion3 = new QuizQuestion(
-                    "Ile jest 4+4",
-                    0,
-                    List.of(
-                            "16",
-                            "1",
-                            "3",
-                            "-16"
-                    ),
-                    "Matematyka");
+            list.add(new QuizQuestion(
+                    "Ile jest 23+2?", 0, List.of("25", "1", "0", "4"), "matematyka"));
 
-            QuizQuestion quizQuestion4 = new QuizQuestion(
-                    "Jaki jest wzór na pole koła",
-                    0,
-                    List.of(
-                            "pi r ^2",
-                            "2 pi r",
-                            "a+b",
-                            "1/4 pi^4"
-                    ),
-                    "Matematyka");
+            list.add(new QuizQuestion(
+                    "Jaki jest wzór na pole kwadratu", 0, List.of("a^2", "a^3", "a+b", "(a+b)^2"), "matematyka"));
 
-            QuizQuestion quizQuestion5 = new QuizQuestion(
-                    "Jaki jest wzór na pole rombu",
-                    0,
-                    List.of(
-                            "e*f/2",
-                            "a^3",
-                            "1/2*pi",
-                            "a+b"
-                    ),
-                    "Matematyka");
+            new QuizQuestion(
+                    "Jaki jest wzór na pole kwadratu", 0, List.of("a^2", "a^3", "a+b", "(a+b)^2"), "matematyka");
 
-            QuizQuestion quizQuestion6 = new QuizQuestion(
-                    "Co jest stolicą Hiszpani",
-                    2,
-                    List.of(
-                            "Barcelona",
-                            "Monako",
-                            "Madryt",
-                            "Lizbona"
-                    ),
-                    "Geografia");
 
-            quizQuestionRepository.saveAll(
-                    List.of(
-                            quizQuestion1,
-                            quizQuestion2,
-                            quizQuestion3,
-                            quizQuestion4,
-                            quizQuestion5,
-                            quizQuestion6));
+            quizQuestionRepository.saveAll(list);
+
         };
     }
 }
